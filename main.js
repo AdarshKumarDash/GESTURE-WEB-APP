0018ec3759a544895a4fb701b5497d36f1993a72
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-03-16 20:17:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-//https://teachablemachine.withgoogle.com/models/woCWzcmZT/
+//https://teachablemachine.withgoogle.com/models/pHdVwXaq3/
+
+Webcam.set({
+    height: 280,
+    width: 370,
+    image_format: "png",
+    png_quality: 100
+});
+camera = document.getElementById("camera");
+Webcam.attach("camera");
+
+function capture() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = "<img id='captured_image' src=" + data_uri + ">";
+    });
+}
+console.log("ml5version", ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/6B5uOW5Dy/model.json", modeluploaded);
+
+function modeluploaded() {
+    console.log("model Loaded");
+}
